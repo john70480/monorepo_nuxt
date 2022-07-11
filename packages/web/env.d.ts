7a1f2d82adf979854cbaab6b4437d5abf4347d4a
Edison <edison.lin@gdlink.tw>
2022-07-11 15:42:26 +0800

    chore:測試圖片路徑寫法

--- packages/web/env.d.ts
+++ packages/web/env.d.ts
@@ -1,6 +1,7 @@
 import { defineTgApi } from '@tg/api';
 
-declare module 'nuxt/dist/app/nuxt' {
+// declare module 'nuxt/dist/app/nuxt' {
+declare module '#app' {
 	export interface NuxtApp {
 		$api: () => ReturnType<typeof defineTgApi>
 	}
--- packages/web/pages/index.vue
+++ packages/web/pages/index.vue
@@ -1,4 +1,16 @@
 <template>
-	test-web
+
+	<div>
+		<span>test-web</span>
+		<!-- nuxt路徑 -->
+		<img src="~/assets/images/chess_banner_img_1.png" />
+		<img src="images/chess_banner_img_1.png" />
+		<!-- /@tg/web路徑 -->
+		<img src="@tg/web/assets/images/chess_banner_img_1.png" />
+		<!-- /當前目錄 -->
+		<img src="./chess_banner_img_1.png" />
+		<!-- public -->
+		<img src="/images/chess_banner_img_1.png" />
+	</div>
 </template>
 <!-- export { default } from './fake/index.vue'; -->
