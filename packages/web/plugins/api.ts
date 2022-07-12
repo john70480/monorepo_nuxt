ea0b1c14fb6b54e5737ea6f17e6d6e47a3a8f4e8
Edison <edison.lin@gdlink.tw>
2022-07-12 09:41:06 +0800

    chore:api 改回

--- packages/web/plugins/api.ts
+++ packages/web/plugins/api.ts
@@ -7,36 +7,31 @@ export default defineNuxtPlugin(nuxtApp => {
 	// now available on `nuxtApp.$injected`
 	// nuxtApp.provide('example', () => 'this is example code')
 	nuxtApp.provide('api', () => {
-		return api()
+		const config = useRuntimeConfig();
+		const tgApi = defineTgApi({
+			getBaseUrl: () => config.TG_RELAY_API_URL,
+			getHeaders: () => {
+				const platform = usePlatform();
+				const session = useSession();
+				return {
+					platform: 'Web_1.0',
+					ticket: session.user?.ticket,
+					username: session.user?.username,
+					vga: platform.vga,
+					deviceInfo: platform.deviceInfo,
+					domain: platform.domain,
+					uuid: platform.uuid,
+					Lang: 'zh-cn',
+				}
+			},
+			getEncryptUserId: () => {
+				const platform = usePlatform();
+				if (platform.isEncrypt) {
+					const session = useSession();
+					return session.userId;
+				}
+			},
+		})
+		return tgApi;
 	})
 })
-
-export const api = () => {
-	const config = useRuntimeConfig();
-	const tgApi = defineTgApi({
-		getBaseUrl: () => config.TG_RELAY_API_URL,
-		getHeaders: () => {
-			const platform = usePlatform();
-			const session = useSession();
-			return {
-				platform: 'Web_1.0',
-				ticket: session.user?.ticket,
-				username: session.user?.username,
-				vga: platform.vga,
-				deviceInfo: platform.deviceInfo,
-				domain: platform.domain,
-				uuid: platform.uuid,
-				Lang: 'zh-cn',
-			}
-		},
-		getEncryptUserId: () => {
-			const platform = usePlatform();
-			if (platform.isEncrypt) {
-				const session = useSession();
-				return session.userId;
-			}
-		},
-	})
-	return tgApi;
-
-}
