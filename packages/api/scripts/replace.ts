ce197ab688b0c7625ca7fb19982a3d4f8ce85805
Edison <edison.lin@gdlink.tw>
2022-08-01 18:20:52 +0800

    feat:新增api格式replace流程

---
+++ packages/api/scripts/replace.ts
@@ -0,0 +1,31 @@
+import { readFile, writeFile } from 'fs';
+
+(async () => {
+  const fileList=[
+    'Service',
+    'IpService'
+  ]
+  fileList.forEach(file=>{
+    readFile(`./src/tg/services/${file}.ts`, 'utf-8', function(err, contents) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      const replaced = contents.
+      replace(/payload\?\: \{/g, 'Payload?: {').
+      replace(/statusCode\?\: number/g, 'StatusCode?: number').
+      replace(/message\?: string;/g, 'Message?: string;');
+      
+
+      writeFile(`./src/tg/services/${file}.ts`, replaced, 'utf-8', function(err) {
+        if (err) {
+          console.log(err);
+        }else{
+          console.log(`api ${file}.ts replace success`);      
+        }
+      });
+    });    
+  })
+
+})();
