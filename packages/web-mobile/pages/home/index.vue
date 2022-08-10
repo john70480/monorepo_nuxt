50665e919197f0168e05746cf67edc38c8b57ac6
Edison <edison.lin@gdlink.tw>
2022-08-10 10:32:50 +0800

    feat:新增swiper套件做輪播效果

--- packages/web-mobile/pages/home/index.vue
+++ packages/web-mobile/pages/home/index.vue
@@ -16,53 +16,39 @@
 
 		<SubTitle type="game" title="热门赛事"></SubTitle>
 		<div class="index_box">
-			<div class="swiper-container game-swiper swiper-container-initialized swiper-container-horizontal">
-				<div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(0px, 0px, 0px);">
-					<div class="swiper-slide" v-for="n in 5" :key="n">
-						<MatchCard></MatchCard>
-					</div>
-				</div>
-			</div>
+			<Swiper class="swiper-container game-swiper " :autoplay="true" slides-per-view="auto">
+				<SwiperSlide v-for="n in 5" :key="n">
+					<MatchCard></MatchCard>
+				</SwiperSlide>
+			</Swiper>
 		</div>
 
 		<SubTitle type="event" title="优惠活动" :showFilter="true"></SubTitle>
 		<div class="index_box">
-			<div class="swiper-container event-swiper swiper-container-initialized swiper-container-horizontal">
-				<div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(0px, 0px, 0px);">
-					<div class="swiper-slide" v-for="n in 5" :key="n">
-						<img src="@tg/web-mobile/assets/images/banner02.png" />
-					</div>
-				</div>
-			</div>
+			<Swiper class="swiper-container event-swiper " :autoplay="true" slides-per-view="auto">
+				<SwiperSlide v-for="n in 5" :key="n">
+					<img src="@tg/web-mobile/assets/images/banner02.png" />
+				</SwiperSlide>
+			</Swiper>
 		</div>
 
 		<SubTitle type="live" title="赛事直播"></SubTitle>
 		<div class="index_box">
-			<div class="swiper-container live-swiper swiper-container-initialized swiper-container-horizontal">
-				<div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(0px, 0px, 0px);">
-					<div class="swiper-slide" v-for="n in 2" :key="n">
-						<div class="game_live">
-							<div class="video">
-								<iframe src="https://www.youtube.com/embed/ZEQdAVoh_ww" frameborder="0"></iframe>
-							</div>
-							<div class="team">华沙普洛克U19(主)</div>
-							<div class="team">洛兹U19</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<Swiper class="swiper-container live-swiper " :autoplay="true" slides-per-view="auto">
+				<SwiperSlide v-for="n in 5" :key="n">
+					<VideoCard></VideoCard>
+				</SwiperSlide>
+			</Swiper>
 		</div>
 
 		<template v-for="(item, index) in platformList" :key="index">
 			<SubTitle :type="index" :title="item.title"></SubTitle>
 			<div class="index_box">
-				<div class="swiper-container platform-swiper swiper-container-initialized swiper-container-horizontal">
-					<div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(0px, 0px, 0px);">
-						<div v-for="(list, index) in item.list" :key="index" class="swiper-slide">
-							<PlatformCard :title="list.title" :ImageSrc="list.imageSrc"></PlatformCard>
-						</div>
-					</div>
-				</div>
+				<Swiper class="swiper-container platform-swiper " :autoplay="true" slides-per-view="auto">
+					<SwiperSlide v-for="(list, index) in item.list" :key="index">
+						<PlatformCard :title="list.title" :ImageSrc="list.imageSrc"></PlatformCard>
+					</SwiperSlide>
+				</Swiper>
 			</div>
 		</template>
 
@@ -70,11 +56,13 @@
 </template>
 <script setup lang="ts">
 import { useApp } from '@tg/web-mobile/stores/app';
+import { Swiper, SwiperSlide } from 'swiper/vue';
 import Banner from "./banner.vue";
 import Announcement from './announcement.vue';
 import SubTitle from './sub-ttitle.vue';
 import PlatformCard from './platform-card.vue';
 import MatchCard from './match-card.vue';
+import VideoCard from './video-card.vue';
 
 definePageMeta({
 	hiddenHeader: true
@@ -312,43 +300,6 @@ const platformList = {
 					position: relative;
 					transition-property: transform;
 				}
-			}
-		}
-	}
-
-
-	.game_live {
-		width: 100%;
-
-		.video {
-			width: 100%;
-			height: 100px;
-			border-radius: 5px;
-			margin-bottom: 10px;
-			background: url('@tg/web-mobile/assets/images/live_stadium_img.png') center no-repeat, #b0d8ff;
-			background-size: cover;
-			overflow: hidden;
-
-			iframe {
-				width: 100%;
-				height: 100%;
-			}
-		}
-
-		.team {
-			color: #ffffff;
-			font-size: 0.7rem;
-			margin-bottom: 3px;
-			display: flex;
-			align-items: center;
-
-			&::before {
-				content: '';
-				width: 8px;
-				height: 8px;
-				margin-right: 5px;
-				border-radius: 50%;
-				background: linear-gradient(to bottom, #eaf5fb, #b2c9d5);
 			}
 		}
 	}
---
+++ packages/web-mobile/pages/home/video-card.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="game_live">
+		<div class="video">
+			<iframe src="https://www.youtube.com/embed/ZEQdAVoh_ww" frameborder="0"></iframe>
+		</div>
+		<div class="team">华沙普洛克U19(主)</div>
+		<div class="team">洛兹U19</div>
+	</div>
+
+</template>
+
+<style lang="scss" scoped>
+.game_live {
+	width: 100%;
+
+	.video {
+		width: 100%;
+		height: 100px;
+		border-radius: 5px;
+		margin-bottom: 10px;
+		background: url('@tg/web-mobile/assets/images/live_stadium_img.png') center no-repeat, #b0d8ff;
+		background-size: cover;
+		overflow: hidden;
+
+		iframe {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.team {
+		color: #ffffff;
+		font-size: 0.7rem;
+		margin-bottom: 3px;
+		display: flex;
+		align-items: center;
+
+		&::before {
+			content: '';
+			width: 8px;
+			height: 8px;
+			margin-right: 5px;
+			border-radius: 50%;
+			background: linear-gradient(to bottom, #eaf5fb, #b2c9d5);
+		}
+	}
+}
+</style>
