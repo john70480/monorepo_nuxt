e7cb1f20f42801516996476f7180591aa8a9789b
Edison <edison.lin@gdlink.tw>
2022-08-16 10:50:16 +0800

    chore:slider-unlock改寫成vue3

--- packages/web-mobile/pages/login/slider-unlock.vue
+++ packages/web-mobile/pages/login/slider-unlock.vue
@@ -1,105 +1,98 @@
 <template>
-	<div id="box">
-		<div class="bgColor"></div>
-		<div class="txt">请滑动验证</div>
+	<div id="box" ref="box">
+		<div class="bgColor" ref="bgColor"></div>
+		<div class="txt">{{ txt }}</div>
 		<!--給i標簽添加上相應字體圖標的類名即可-->
-		<div class="slider">
+		<div class="slider" ref="slider" @mousedown="mousedownHandler" @touchstart="mousedownHandler">
 			<i v-show="!isSuccess" class="el-icon-d-arrow-right"></i>
 			<i v-show="isSuccess" class="el-icon-check"></i>
 		</div>
 	</div>
 </template>
-<script  >
-export default {
-	mounted() {
-		var self = this;
-		//一、定義瞭一個獲取元素的方法
-		function getEle(selector) {
-			return document.querySelector(selector);
-		}
-		//二、獲取到需要用到的DOM元素
-		var box = getEle("#box"), //容器
-			bgColor = getEle(".bgColor"), //背景色
-			txt = getEle(".txt"), //文本
-			slider = getEle(".slider"), //滑塊
-			icon = getEle(".slider>i"),
-			successMoveDistance = box.offsetWidth - slider.offsetWidth, //解鎖需要滑動的距離
-			downX; //用於存放鼠標按下時的位置
-		//三、給滑塊添加鼠標按下事件
-		slider.onmousedown = mousedownHandler;
-		slider.ontouchstart = mousedownHandler; //移動端加touchstart事件
-		//3.1鼠標按下事件的方法實現
-		function mousedownHandler(e) {
-			bgColor.style.transition = "";
-			slider.style.transition = "";
-			var e = e || window.event || e.which;
-			downX = e.clientX ? e.clientX : e.changedTouches[0].clientX;
-			if (!self.isSuccess) {
-				//在鼠標按下時，分別給鼠標添加移動和松開事件
-				document.onmousemove = mousemoveHandler;
-				document.onmouseup = mouseupHandler;
-				//添加移動端對應事件
-				document.ontouchmove = mousemoveHandler;
-				document.ontouchend = mouseupHandler;
-			}
-		}
-		//四、定義一個獲取鼠標當前需要移動多少距離的方法
-		function getOffsetX(offset, min, max) {
-			if (offset < min) {
-				offset = min;
-			} else if (offset > max) {
-				offset = max;
-			}
-			return offset;
-		}
-		//3.1.1鼠標移動事件的方法實現
-		function mousemoveHandler(e) {
-			var e = e || window.event || e.which;
-			var moveX = e.clientX ? e.clientX : e.changedTouches[0].clientX;
-			console.log(moveX);
-			var offsetX = getOffsetX(moveX - downX, 0, successMoveDistance);
-			bgColor.style.width = offsetX + "px";
-			slider.style.left = offsetX + "px";
+<script setup lang="ts">
 
-			if (offsetX == successMoveDistance) {
-				success();
-			}
-			//如果不設置滑塊滑動時會出現問題（目前還不知道為什麼）
-			e.preventDefault();
-		}
-		//3.1.2鼠標松開事件的方法實現
-		function mouseupHandler(e) {
-			if (!self.isSuccess) {
-				bgColor.style.width = 0 + "px";
-				slider.style.left = 0 + "px";
-				bgColor.style.transition = "width 0.5s linear";
-				slider.style.transition = "left 0.5s linear";
-			}
-			document.onmousemove = null;
-			document.onmouseup = null;
-			//移除移動端事件
-			document.ontouchmove = null;
-			document.ontouchend = null;
-		}
-		//五、定義一個滑塊解鎖成功的方法
-		function success() {
-			self.isSuccess = true;
-			txt.innerHTML = "解鎖成功";
-			bgColor.style.backgroundColor = "lightgreen";
-			//滑動成功時，移除鼠標按下事件和鼠標移動事件
-			slider.onmousedown = null;
-			document.onmousemove = null;
-			//移除移動端事件
-			document.ontouchstart = null;
-			document.ontouchmove = null;
-		}
-	},
-	data() {
-		return {
-			isSuccess: false,
-		};
-	},
-};
+const isSuccess = ref(false)
+//二、獲取到需要用到的DOM元素
+const box = ref<HTMLElement>(); //容器
+const bgColor = ref<HTMLElement>(); //背景色
+const txt = ref('请滑动验证'); //文本
+const slider = ref<HTMLElement>(); //滑塊
+const successMoveDistance = computed(() => (box.value && slider.value) ? box.value.offsetWidth - slider.value.offsetWidth : 0); //解鎖需要滑動的距離
+let downX: number; //用於存放鼠標按下時的位置
+//三、給滑塊添加鼠標按下事件
+//3.1鼠標按下事件的方法實現
+function mousedownHandler(e: MouseEvent | TouchEvent) {
+	if (bgColor.value && slider.value) {
+		bgColor.value.style.transition = "";
+		slider.value.style.transition = "";
+	}
+	var e = e || window.event || e.which;
+	// debugger
+	downX = e.clientX ? e.clientX : e.changedTouches[0].clientX;
+	if (!isSuccess.value) {
+		//在鼠標按下時，分別給鼠標添加移動和松開事件
+		document.onmousemove = mousemoveHandler;
+		document.onmouseup = mouseupHandler;
+		//添加移動端對應事件
+		document.ontouchmove = mousemoveHandler;
+		document.ontouchend = mouseupHandler;
+	}
+}
+//3.1.1鼠標移動事件的方法實現
+function mousemoveHandler(e: MouseEvent | TouchEvent) {
+	var e = e || window.event || e.which;
+	var moveX = e.clientX ? e.clientX : e.changedTouches[0].clientX;
+	console.log(moveX);
+	var offsetX = getOffsetX(moveX - downX, 0, successMoveDistance.value);
+	if (bgColor.value && slider.value) {
+		bgColor.value.style.width = offsetX + "px";
+		slider.value.style.left = offsetX + "px";
+	}
+
+	if (offsetX == successMoveDistance.value) {
+		success();
+	}
+	//如果不設置滑塊滑動時會出現問題（目前還不知道為什麼）
+	e.preventDefault();
+}
+
+//3.1.2鼠標松開事件的方法實現
+function mouseupHandler(e: MouseEvent | TouchEvent) {
+	if (!isSuccess.value && bgColor.value && slider.value) {
+		bgColor.value.style.width = 0 + "px";
+		slider.value.style.left = 0 + "px";
+		bgColor.value.style.transition = "width 0.5s linear";
+		slider.value.style.transition = "left 0.5s linear";
+	}
+	document.onmousemove = null;
+	document.onmouseup = null;
+	//移除移動端事件
+	document.ontouchmove = null;
+	document.ontouchend = null;
+}
+//四、定義一個獲取鼠標當前需要移動多少距離的方法
+function getOffsetX(offset: number, min: number, max: number) {
+	if (offset < min) {
+		offset = min;
+	} else if (offset > max) {
+		offset = max;
+	}
+	return offset;
+}
+//五、定義一個滑塊解鎖成功的方法
+function success() {
+	isSuccess.value = true;
+	txt.value = "解鎖成功";
+	if (bgColor.value && slider.value) {
+		bgColor.value.style.backgroundColor = "lightgreen";
+		//滑動成功時，移除鼠標按下事件和鼠標移動事件
+		slider.value.onmousedown = null;
+	}
+	document.onmousemove = null;
+	//移除移動端事件
+	document.ontouchstart = null;
+	document.ontouchmove = null;
+}
 </script>
 <style >
 /*  使用全局樣式樣式去掉 */
