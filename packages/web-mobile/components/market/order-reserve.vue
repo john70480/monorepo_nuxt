c670b45ce676c7582e965e3cfa2654b12af4b87f
Edison <edison.lin@gdlink.tw>
2022-10-07 11:58:18 +0800

    chore:調整一般下單&預約下單樣式跟v-text-field改使用tg-text-field

--- packages/web-mobile/components/market/order-reserve.vue
+++ packages/web-mobile/components/market/order-reserve.vue
@@ -2,11 +2,13 @@
 	<v-container class="order order-reserve fill-height overflow-y-auto text-body-2 pa-0 d-flex flex-column" fluid>
 		<v-row class="ma-0 mt-10">
 			<v-col cols="12">
-				<span>你正在</span>
-				<span class="text-red">反對</span>
-			</v-col>
-			<v-col cols="12" class="select-name">
-				<b class="text-red">波胆1-0</b>
+				<p>
+					<span>你正在</span>
+					<span class="text-red">反對</span>
+				</p>
+				<p class="select-name pt-1">
+					<b class="text-red">波胆1-0</b>
+				</p>
 			</v-col>
 		</v-row>
 
@@ -15,36 +17,43 @@
 				<b>欧洲冠军联赛</b>
 				<span>2020-10-15 23:30</span>
 			</v-col>
-			<v-col cols="12">
-				<div class="team_row">
-					<div class="team_name">
-						<b>华沙普洛克U19(主)</b>
-						<b>洛兹U19</b>
-					</div>
-					<div class="guaranteed">保本</div>
+			<v-col cols="12" class="team_row ">
+				<div class="team_name">
+					<b>华沙普洛克U19(主)</b>
+					<b>洛兹U19</b>
 				</div>
+				<div class="guaranteed">保本</div>
 			</v-col>
 		</v-row>
 
 		<v-row class="ma-0">
 			<v-col cols="6">
 				<b>交易金额</b>
-				<p><b>*保本额度:9,999,999.00</b></p>
+				<p><b>*尚可预约笔数:50</b></p>
 			</v-col>
-			<v-col cols="6">
-				<v-text-field v-model="amount" :counter="10" label="交易金额" required></v-text-field>
+			<v-col cols="6" class="d-flex align-center">
+				<tg-text-field v-model="amount"></tg-text-field>
 			</v-col>
 			<v-col cols="12" class="setting d-flex align-center justify-end ">
 				<img src="@tg/web-mobile/assets/images/com_body_setting_ic.svg" />
 				<v-btn v-for="item in 3">{{ item }}</v-btn>
 			</v-col>
 			<v-col cols="12" class="d-flex justify-space-between">
-				<b>获利%</b>
+				<b>目前获利%</b>
 				<b>12.6%</b>
 			</v-col>
-			<v-col cols="12" class="d-flex justify-space-between">
+			<v-col cols="4">
 				<b>获利区间</b>
-				<b class="text-red">-5%</b>
+			</v-col>
+			<v-col cols="8" class="d-flex align-center">
+				<tg-text-field></tg-text-field>
+				<span>%~</span>
+				<tg-text-field></tg-text-field>
+				<span>%</span>
+			</v-col>
+			<v-col cols="12" class="pt-0 pb-0 ">
+				<p class="text-red">(如赔率有变动，在此区间内暨同意下单)</p>
+				<p>获利%已从<span class="text-red">##.00%</span>变更为<span class="text-red">##.00%</span>，请重新设定区间</p>
 			</v-col>
 			<v-col cols="12" class="d-flex justify-space-between">
 				<b>手续费</b>
@@ -57,23 +66,24 @@
 
 		</v-row>
 
-		<v-row class="ma-0" no-gutters>
+		<v-row class="pa-2 align-content-space-between">
 			<v-col cols="12">
-				<v-col cols="12">
+				<p>
 					<span>请确认交易明细无误，确认交易后</span>
 					<span class="text-red">无法改注单</span>
-				</v-col>
-				<v-col cols="12" class="balance">
+				</p>
+				<p class="balance">
 					<img src="@tg/web-mobile/assets/images/tg_money_ic.svg" />
 					<b>TG余额:3,168,500.00</b>
-				</v-col>
+				</p>
 			</v-col>
-			<v-btn block color="primary">确定预约</v-btn>
+			<v-col cols="12">
+				<v-btn block color="primary">确定预约</v-btn>
+			</v-col>
 		</v-row>
 	</v-container>
 </template>
 <script lang="ts" setup>
-const items = [1, 2, 3];
 const amount = ref<number>();
 </script>
 <style lang="scss" scoped>
