c4b1e8543d316f8957b7e5475d497cd85b571415
Edison <edison.lin@gdlink.tw>
2022-10-13 16:16:37 +0800

    feat:tgTabs新增樣式 market-list-tabs

--- packages/web-mobile/components/tg/tabs.vue
+++ packages/web-mobile/components/tg/tabs.vue
@@ -1,14 +1,17 @@
 <template>
-	<v-tabs v-model="modelValueProxy" class="tabs" :type="type" align-with-title>
+	<v-tabs v-model="modelValueProxy" :type="type" v-bind="_bind">
 		<slot></slot>
 		<slot name="filter"></slot>
 	</v-tabs>
 </template>
 
 <script lang="ts" setup>
+import type { VTabs } from "vuetify/components";
+
 const props = defineProps<{
 	type: typeof types[number],
 	modelValue?: any,
+	tabsBind?: VTabs["$props"]
 }>();
 
 const emit = defineEmits<{
@@ -17,6 +20,15 @@ const emit = defineEmits<{
 const modelValueProxy = computed({
 	get: () => props.modelValue,
 	set: (val) => emit("update:modelValue", val)
+})
+const _bind = computed(() => {
+	let obj: VTabs["$props"] = {}
+	switch (props.type) {
+		default:
+			obj = defaultBind;
+			break;
+	}
+	return { ...obj, ...props.tabsBind }
 })
 </script>
 
@@ -27,10 +39,14 @@ export default {
 
 export const types = [
 	'tg-tabs',
+	'market-list-tabs',
 ] as const;
+export const defaultBind: VTabs["$props"] = {
+	alignWithTitle: true,
+	hideSlider: true
+}
 </script>
 <style lang="scss" >
-// <style lang="scss" module>
 %flex-tabs {
 	display: inline-flex;
 	flex-wrap: nowrap;
@@ -56,7 +72,7 @@ export const types = [
 	}
 }
 
-.tabs[type="tg-tabs"] {
+.v-tabs[type="tg-tabs"] {
 	@extend %flex-tabs;
 	background: #36567f;
 	display: flex;
@@ -96,6 +112,27 @@ export const types = [
 				border-radius: 50%;
 				background: linear-gradient(to bottom, #ffae5c, #f5720d);
 			}
+		}
+	}
+}
+
+.v-tabs[type="market-list-tabs"] {
+	@extend %flex-tabs;
+	display: flex;
+	align-items: center;
+	overflow-x: auto;
+	height: fit-content;
+
+	.v-tab {
+		min-width: 30px;
+		height: 20px !important;
+		color: #c3c3c3;
+		margin: 0px;
+		padding: 0px;
+
+		&.v-slide-group-item--active,
+		&:hover {
+			color: #283763;
 		}
 	}
 }
