ae88feded0cd604943bf337478c240d05adcf623
Edison <edison.lin@gdlink.tw>
2022-11-14 18:03:32 +0800

    chore(web-mobile):修改分類改成吃api的資料跟圖片

--- packages/web-mobile/components/tg/classification/index.vue
+++ packages/web-mobile/components/tg/classification/index.vue
@@ -7,15 +7,16 @@
 				</v-tab>
 			</TgTabs>
 			<v-container class="px-0 pt-1 overflow-y-auto flex-1">
-				<v-row class="classification-wrap" v-for=" (item, index) in platformList" :key="index" :dense="true">
+				<v-row class="classification-wrap" v-for=" (item, index) in siteList?.categories" :key="index" :dense="true">
 					<v-col class="classification-title text-center" cols="12">
-						<b>{{ item.title }}</b>
+						<b>{{ item.thirdPartyCategory }}</b>
 					</v-col>
 					<v-col class="classification-content px-6 py-4" cols="12">
 						<v-row>
-							<TgItemImage v-for="(list, index) in item.list" :key="index" :imgBind="{ src: list.imageSrc }"
-								contentClass="mx-1 pa-2" class="px-2 py-2">
-								{{ list.title }}
+							<TgItemImage v-for="(list, index) in siteList?.platform?.[item.thirdPartyType]?.[item.thirdPartyCategory]"
+								:key="index" :imgBind="{ src: platformMap?.[list.thirdPartyCode]?.banner }" contentClass="mx-1 pa-2"
+								class="px-2 py-2">
+								{{ list.thirdPartyName }}
 							</TgItemImage>
 						</v-row>
 					</v-col>
@@ -27,9 +28,13 @@
 <script lang="ts" setup>
 import { platformList } from '@tg/web-mobile/pages/home/index.vue';
 import { useDialogs } from '@tg/web-mobile/stores/dialogs';
+import { useHome, getSiteListMap } from '@tg/web-mobile/stores/home';
 
 const dialogsStore = useDialogs();
+const homeStore = useHome();
 const platformTab = ref<keyof typeof platformList>('sports')
+const siteList = await homeStore.getSiteList();
+const { platformMap, categoryMap } = getSiteListMap();
 </script>
 <style lang="scss" scoped>
 .classification-wrap {
