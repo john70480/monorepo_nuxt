a8f82eccdff4ef32ea80b352df4c12fbb1e8a2b2
zoe.chen3 <zoe.chen@gdlink.tw>
2023-01-12 14:03:21 +0800

    feat(web-mobile):新增幫助中心頁面與調整item-btn元件

---
+++ packages/web-mobile/components/tg/item-btn.vue
@@ -0,0 +1,87 @@
+<template>
+    <nav class="item-box">
+
+    
+        <div class="item-btn" :type="type">
+
+            <v-btn v-for="(item, index) in list" v-bind="_bind" :class="{ target: modelValueProxy == index }"
+                class="btnClass" @click="modelValueProxy = index">{{
+        item
+                }}</v-btn>
+        </div>
+    </nav>
+</template>
+
+
+<script lang="ts" setup>
+
+import type { VBtn } from "vuetify/components";
+
+
+const props = withDefaults(defineProps<{
+    type?: typeof types[number],
+    modelValue?: any,
+    list: Array<any>,
+    btnBind?: VBtn["$props"]
+}>(), {
+    type: 'default',
+});
+const emit = defineEmits<{
+    (event: 'update:modelValue', payload?: any): void
+}>();
+const modelValueProxy = computed({
+    get: () => props.modelValue,
+    set: (val) => emit("update:modelValue", val)
+})
+
+const _bind = computed(() => {
+    let obj: VBtn["$props"] = {}
+    switch (props.type) {
+        default:
+            obj = defaultBind;
+            break;
+    }
+    return { ...obj, ...props.btnBind }
+})
+</script>
+
+<script lang="ts">
+export const types = [
+    'default',
+] as const;
+export const defaultBind: VBtn["$props"] = {
+    variant: 'outlined',
+}
+</script>
+
+<style lang="scss" scoped>
+.item-box{
+    overflow-x: auto;
+    -webkit-overflow-scrolling: touch;
+    height: 54px;
+    white-space: nowrap;
+    padding: 12px;
+    background-color: #587eb0;
+
+    .item-btn[type="default"] {
+        .btnClass {
+            display: inline-block;
+            height: 30px;
+            font-size: 0.8rem;
+            padding-left: 15px;
+            padding-right: 15px;
+            margin-left: 10px;
+            background-color: #587eb0;
+            border: 1px solid #fff;
+            color: #fff;
+            border-radius: 50px;
+
+            &.target {
+                background-color: #36567f;
+                border: 1px solid #36567f;
+                color: #fff;
+            }
+        }
+    }
+}
+</style>
