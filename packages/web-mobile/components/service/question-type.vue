fc757d2b1c6ab9c809248f5781d48395208a3747
zoe.chen3 <zoe.chen@gdlink.tw>
2023-01-16 15:34:48 +0800

    feat(web-mobile): 新增線上客服頁面

---
+++ packages/web-mobile/components/service/question-type.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card>
+    <v-card-title>问题选择</v-card-title>
+    <v-card-text class="mx-1 px-1">
+      <v-list-item v-for="item in questionList" class="question_item">
+        <template v-slot:prepend="{ isActive }">
+          <v-list-item-action start>
+            <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
+          </v-list-item-action>
+        </template>
+        <v-list-item-title>{{ item.text }}</v-list-item-title>
+      </v-list-item>
+    </v-card-text>
+    <v-card-actions class="pb-8">
+      <v-btn block class="btn-submit bg-primary">確認</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script lang="ts" setup>
+import { Ref } from "vue";
+
+const questionList = ref([
+  {
+    text: "实名/资料变更",
+    value: 0,
+  },
+  {
+    text: "充值/提领/余额",
+    value: 1,
+  },
+  {
+    text: "体育赛事/娱乐游戏",
+    value: 2,
+  },
+  {
+    text: "VIP等级/微商等级",
+    value: 3,
+  },
+  {
+    text: "平台优惠活动",
+    value: 4,
+  },
+  {
+    text: "其它问题",
+    value: 99,
+  },
+]);
+</script>
+
+<style lang="scss" scoped>
+.question_item{
+    border-bottom: solid 1px #c3c3c3;
+    padding: 4px;
+    margin: 0px;
+}
+</style>
---
+++ packages/web-mobile/pages/service/index.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="service_view">
+        <div class="service_card online" @click="() => router.push('/service/online')">
+            <div class="service_icon">
+                <img src="../../assets/images/com_body_24h_ic_small.png"/>
+            </div>
+            <div class="service_content">
+                <b>在线客服</b><br/>
+                点击直接对话
+            </div>
+        </div>
+        <div class="service_card qq">
+            <div class="service_icon">
+                <img src="../../assets/images/com_body_qq_ic_small.png"/>
+            </div>
+            <div class="service_content">
+                <b>QQ客服</b><br/>
+                QQ号/123546879
+            </div>
+        </div>
+        <div class="service_card wechat">
+            <div class="service_icon">
+                <img src="../../assets/images/com_body_wechat_ic_small.png"/>
+            </div>
+            <div class="service_content">
+                <b>微信客服</b><br/>
+                微信号/123546879
+            </div>
+        </div>
+  </div>
+</template>
+<script setup lang="ts">
+const router = useRouter();
+definePageMeta({
+  title: "24H线上客服",
+});
+
+
+</script>
+<style lang="scss" scoped>
+.service_view {
+    height: 100vh;
+    background: var(--tg-bg-primary);
+    padding: 12px 32px;
+  .service_card{
+    height: 68px;
+    width: 100%;
+    border-radius: 4px;
+    display: flex;
+    align-items:center;
+    margin-bottom: 12px;
+    &.online{
+        background-image: linear-gradient(to bottom, #f1a5c3, #af2e91);
+    }
+    &.qq{
+        background-image: linear-gradient(to bottom, #65ceff , #1944b9);
+    }
+    &.wechat{
+        background-image: linear-gradient(to bottom, #b0f8f8, #037868);
+    }
+    .service_icon{
+        width: 64px;
+        padding-left: 24px;
+        img{
+            height: 48px;
+        }
+    }
+    .service_content{
+        flex-grow:1;
+        text-align: center;
+        color:#fff;
+    }
+  }
+  
+}
+</style>
